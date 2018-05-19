9235163b87d700687e8ccbdab1473643e4185b46
Shompa <nico_nahuelpan@hotmail.com>
2018-05-19 14:43:08 -0400

    buscador completo

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html'
 })
 export class SearchComponent {
+  termino:string = undefined;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute, private _ps:ProductosService) {
+    route.params.subscribe( params=>{
+      this.termino = params['termino'];
+
+      _ps.buscarProducto(this.termino)
+    })
+  }
 
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,6 +8,7 @@ import { Http } from '@angular/http';
 export class ProductosService {
 
   productos:any[] = [];
+  productosFiltrados:any[] = [];
   cargando:boolean = true;
 
   constructor( private http:Http ) {
@@ -15,12 +16,40 @@ export class ProductosService {
   }
 
   public cargarProductos(){
-    this.cargando = true;
-        this.http.get("https://portafolio-7e9be.firebaseio.com/productos_idx.json")
-                .subscribe( res => {
-                  this.productos = res.json();
-                  this.cargando = false;
-                })
+
+    let promesa = new Promise(( resolve, reject) =>{
+      this.cargando = true;
+          this.http.get("https://portafolio-7e9be.firebaseio.com/productos_idx.json")
+                  .subscribe( res => {
+                    this.productos = res.json();
+                    this.cargando = false;
+                    resolve();
+                  });
+    }) ;
+    return promesa;
+  }
+
+  public buscarProducto(termino:string){
+
+    if (this.productos.length === 0) {
+        this.cargarProductos().then(()=>{
+          this.filtrarProductos(termino);
+        });
+    }else{
+      this.filtrarProductos(termino);
+    }
+  }
+
+  private filtrarProductos(termino:string){
+    this.productosFiltrados = [];
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod =>{
+
+      if (prod.categoria.indexOf( termino ) >=0 || prod.titulo.toLowerCase().indexOf( termino ) >=0) {
+          this.productosFiltrados.push(prod);
+      }
+    })
   }
 
   public cargarProducto( cod:string ){
