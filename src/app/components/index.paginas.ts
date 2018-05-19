8df789d3858a8dafdffe68dcfaae22b4210214b8
Shompa <nico_nahuelpan@hotmail.com>
2018-05-19 10:23:34 -0400

    rutas agregadas

---
+++ src/app/components/index.paginas.ts
@@ -0,0 +1,3 @@
+export { PortafolioComponent } from './portafolio/portafolio.component';
+export { ProductoComponent } from './producto/producto.component';
+export { AboutComponent } from './about/about.component';
